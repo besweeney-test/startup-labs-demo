d29861b08158b8414abceed8d775576d2d80e689
besweeney <besweeney@google.com>
2024-07-25 20:30:15 -0700

    automated change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React, or don't!! WooHoo automatic changes
         </a>
       </header>
     </div>
